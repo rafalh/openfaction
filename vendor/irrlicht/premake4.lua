764eb1ab256622c3c76d0903822b80f0fde42924
rafalh1992 <rafalh@users.noreply.github.com>
2012-06-22 22:25:47 +0000

    - Add vendor files, part 2

---
+++ vendor/irrlicht/premake4.lua
@@ -0,0 +1,25 @@
+	project "Irrlicht"
+	kind "SharedLib"
+	
+	includedirs {
+		"include",
+		"src",
+		"../zlib",
+	}
+	
+	files {
+		"src/*.cpp",
+		"src/lzma/*.c",
+		"src/aesGladman/*.cpp",
+	}
+	
+	defines { "IRRLICHT_EXPORTS" }
+	
+	links { "kernel32",  "gdi32", "winmm", "opengl32", "bzip2", "jpeglib", "libpng", "zlib" }
+	
+	configuration "Debug"
+		targetdir "../../bin/debug"
+	
+	configuration "Release"
+		targetdir "../../bin/release"
+	
