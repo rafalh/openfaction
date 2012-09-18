af42c86aacb4be93ae0bec0ac02de2df7f6cb59e
rafalh1992 <rafalh@users.noreply.github.com>
2012-09-18 16:49:52 +0000

    - Implement checking if file is a directory in CFileList for Linux

--- include/CFileList.h
+++ include/CFileList.h
@@ -18,6 +18,12 @@
 class CFileList
 {
     public:
+        enum
+        {
+            FILE = 0,
+            DIR = 1,
+        };
+        
         inline CFileList(const char *pPattern)
         {
             m_hFind = FindFirstFileA(pPattern, &m_Wfd);
@@ -40,7 +46,7 @@ class CFileList
                     return -1;
 
                 strName = m_Wfd.cFileName;
-                iRet = (m_Wfd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) ? 1 : 0;
+                iRet = (m_Wfd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) ? DIR : FILE;
 
                 if(!FindNextFileA(m_hFind, &m_Wfd))
                 {
@@ -57,7 +63,7 @@ class CFileList
         WIN32_FIND_DATAA m_Wfd;
 };
 
-#else /* LINUX */
+#else // LINUX
 #include <glob.h>
 
 class CFileList
@@ -65,8 +71,7 @@ class CFileList
     public:
         inline CFileList(const char *pPattern)
         {
-            m_GlobBuf.gl_offs = 0;
-            glob(pPattern, GLOB_DOOFFS, NULL, &m_GlobBuf);
+            glob(pPattern, GLOB_MARK, NULL, &m_GlobBuf);
             m_nCurrentPath = 0;
         }
 
@@ -85,8 +90,17 @@ class CFileList
                 if(m_nCurrentPath >= m_GlobBuf.gl_pathc)
                     return -1;
 
-                strName = m_GlobBuf.gl_pathv[m_nCurrentPath]; // FIXME: is it a full path or what?
-                iRet = 0; // FIXME: check if it is a dir
+                const char *pszFilename = m_GlobBuf.gl_pathv[m_nCurrentPath];
+                if(pszFilename[0] == '/')
+                {
+                    strName = pszFilename + 1;
+                    iRet = DIR;
+                }
+                else
+                {
+                    strName = pszFilename;
+                    iRet = FILE;
+                }
             } while(strName == "." || strName == "..");
 
             return iRet;
@@ -97,6 +111,6 @@ class CFileList
         glob_t m_GlobBuf;
 };
 
-#endif
+#endif // LINUX
 
 #endif // CFILELIST_H
